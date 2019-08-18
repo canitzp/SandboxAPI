5fae2dbf25924c55c2d0a5c8edab0ff3c8ec8053
Coded <coded@hrznstudio.com>
2019-08-18 11:30:07 +0100

    Implement basic addon syncing

--- src/main/java/com/hrznstudio/sandbox/api/event/Event.java
+++ src/main/java/com/hrznstudio/sandbox/api/event/Event.java
@@ -1,4 +1,16 @@
 package com.hrznstudio.sandbox.api.event;
 
 public class Event {
+
+    public boolean isAsync() {
+        return false;
+    }
+
+    public boolean isCancellable() {
+        return false;
+    }
+
+    public void complete() {
+
+    }
 }
