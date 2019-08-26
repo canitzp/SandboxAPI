50e964c1f20058b4bb7a334694d1c52916bb8717
Coded <coded@hrznstudio.com>
2019-08-26 14:00:41 +0100

    :construction: Add BlockItem

---
+++ src/main/java/com/hrznstudio/sandbox/api/item/BlockItem.java
@@ -0,0 +1,17 @@
+package com.hrznstudio.sandbox.api.item;
+
+import com.hrznstudio.sandbox.api.block.Block;
+import com.hrznstudio.sandbox.api.block.IBlock;
+
+public class BlockItem extends Item implements IBlockItem {
+    private final Block block;
+
+    public BlockItem(Block block) {
+        this.block = block;
+    }
+
+    @Override
+    public IBlock asBlock() {
+        return block;
+    }
+}
