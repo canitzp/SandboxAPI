e24394f2970edaf103edf52fc29be06334e9ef22
Coded <coded@hrznstudio.com>
2019-08-26 19:15:36 +0100

    :bug: Fix EnchantmentEvent requiring wrong enchantment type

--- src/main/java/com/hrznstudio/sandbox/api/event/EnchantmentEvent.java
+++ src/main/java/com/hrznstudio/sandbox/api/event/EnchantmentEvent.java
@@ -1,21 +1,25 @@
 package com.hrznstudio.sandbox.api.event;
 
-import com.hrznstudio.sandbox.api.enchant.Enchantment;
+import com.hrznstudio.sandbox.api.enchant.IEnchantment;
 import com.hrznstudio.sandbox.api.item.ItemStack;
 import com.hrznstudio.sandbox.api.util.InteractionResult;
 
 public class EnchantmentEvent extends Event {
-    private final Enchantment enchantment;
+    private final IEnchantment enchantment;
 
-    public EnchantmentEvent(Enchantment enchantment) {
+    public EnchantmentEvent(IEnchantment enchantment) {
         this.enchantment = enchantment;
+    }
+
+    public IEnchantment getEnchantment() {
+        return enchantment;
     }
 
     public static class AcceptableItem extends EnchantmentEvent {
         private final ItemStack stack;
         private InteractionResult result = InteractionResult.IGNORE;
 
-        public AcceptableItem(Enchantment enchantment, ItemStack stack) {
+        public AcceptableItem(IEnchantment enchantment, ItemStack stack) {
             super(enchantment);
             this.stack = stack;
         }
