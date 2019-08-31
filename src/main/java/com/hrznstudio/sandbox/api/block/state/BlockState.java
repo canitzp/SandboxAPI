e4698f82a6ce6a4b773fb1e15f52d1263841cb98
Coded <coded@hrznstudio.com>
2019-08-31 10:35:13 +0100

    :sparkles: Add BlockState utility functions

--- src/main/java/com/hrznstudio/sandbox/api/block/state/BlockState.java
+++ src/main/java/com/hrznstudio/sandbox/api/block/state/BlockState.java
@@ -1,11 +1,30 @@
 package com.hrznstudio.sandbox.api.block.state;
 
 import com.hrznstudio.sandbox.api.block.IBlock;
+import com.hrznstudio.sandbox.api.block.Material;
+import com.hrznstudio.sandbox.api.util.Mirror;
+import com.hrznstudio.sandbox.api.util.Rotation;
 
 public interface BlockState extends PropertyContainer<BlockState> {
     IBlock getBlock();
 
     default boolean isAir() {
         return getBlock().isAir(this);
+    }
+
+    default Material.PistonInteraction getPistonInteraction() {
+        return getBlock().getPistonInteraction(this);
+    }
+
+    default boolean canReplace() {
+        return getBlock().canReplace(this);
+    }
+
+    default BlockState rotate(Rotation rotation) {
+        return getBlock().rotate(this, rotation);
+    }
+
+    default BlockState mirror(Mirror mirror) {
+        return getBlock().mirror(this, mirror);
     }
 }
