53565c6aa0555659c00f9757d85caf3107ca224e
Coded <coded@hrznstudio.com>
2019-10-01 23:49:23 +0100

    :sparkles: Add position validation methods to world

--- src/main/java/org/sandboxpowered/sandbox/api/world/WorldReader.java
+++ src/main/java/org/sandboxpowered/sandbox/api/world/WorldReader.java
@@ -14,4 +14,16 @@ public interface WorldReader {
     BlockEntity getBlockEntity(Position position);
 
     FluidState getFluidState(Position position);
+
+    default boolean isValid(Position position) {
+        return isHeightValid(position) && position.getX() >= -30000000 && position.getZ() >= -30000000 && position.getX() < 30000000 && position.getZ() < 30000000;
+    }
+
+    default boolean isHeightValid(Position position) {
+        return isHeightValid(position.getY());
+    }
+
+    default boolean isHeightValid(int height) {
+        return height > 0 && height < 256;
+    }
 }
