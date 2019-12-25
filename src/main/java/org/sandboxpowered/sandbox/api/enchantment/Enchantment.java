3347f1bcc2c9f85adee9ceaedcc9bf97a4f91328
Coded <coded@voxel.games>
2019-12-25 18:41:57 +0000

    :art: Move enchantment API classes

--- src/main/java/org/sandboxpowered/sandbox/api/enchantment/Enchantment.java
+++ src/main/java/org/sandboxpowered/sandbox/api/enchantment/Enchantment.java
@@ -4,7 +4,10 @@ import org.sandboxpowered.sandbox.api.content.Content;
 import org.sandboxpowered.sandbox.api.entity.Entity;
 import org.sandboxpowered.sandbox.api.entity.LivingEntity;
 import org.sandboxpowered.sandbox.api.item.ItemStack;
+import org.sandboxpowered.sandbox.api.util.annotation.Alpha;
+import org.sandboxpowered.sandbox.api.util.annotation.Beta;
 
+@Beta
 public interface Enchantment extends Content<Enchantment> {
     int getMinimumLevel();
 
@@ -19,15 +22,33 @@ public interface Enchantment extends Content<Enchantment> {
     Weight getWeight();
 
     default void onTargetDamage(LivingEntity self, Entity victim, int level) {
-
     }
 
     default void onUserDamage(LivingEntity self, Entity attacker, int level) {
-
     }
 
     @Override
     default Class<Enchantment> getContentType() {
         return Enchantment.class;
+    }
+
+    enum Target {
+    }
+
+    enum Weight {
+        COMMON(10),
+        UNCOMMON(5),
+        RARE(2),
+        VERY_RARE(1);
+
+        private final int weight;
+
+        Weight(int weight) {
+            this.weight = weight;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
     }
 }
--- src/main/java/org/sandboxpowered/sandbox/api/enchantment/Target.java
+++
@@ -1,6 +0,0 @@
-package org.sandboxpowered.sandbox.api.enchantment;
-
-import org.sandboxpowered.sandbox.api.item.Item;
-
-public enum Target {
-}
