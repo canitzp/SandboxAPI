4bedf21cb3c3ee43c8973f74bcb8267e6899cd88
Meredith Espinosa <goclonefilms@gmail.com>
2020-04-09 02:17:18 -0700

    fix Sandbox

--- base/src/main/java/org/sandboxpowered/api/Sandbox.java
+++ base/src/main/java/org/sandboxpowered/api/Sandbox.java
@@ -29,38 +29,38 @@ public interface Sandbox {
 
 	Identity getPlatform();
 
-	Optional<AddonSpec> getAddon(String addonId);
+	Optional<AddonInfo> getAddon(String addonId);
 
-	Map<AddonSpec, Addon> getAllAddons();
+	Map<AddonInfo, Addon> getAllAddons();
 
-	SandboxAPI getAPIFor(AddonInfo spec);
+	SandboxAPI getAPIFor(AddonInfo info);
 
-	Registrar getRegistrarFor(AddonInfo spec);
+	Registrar getRegistrarFor(AddonInfo info);
 
-	default List<AddonSpec> getLoadOrder() {
-		Set<AddonSpec> visited = new HashSet<>();
-		List<AddonSpec> loadOrder = new ArrayList<>();
-		for (AddonSpec spec : getAllAddons().keySet()) {
-			handleDependencies(visited, loadOrder, spec);
+	default List<AddonInfo> getLoadOrder() {
+		Set<AddonInfo> visited = new HashSet<>();
+		List<AddonInfo> loadOrder = new ArrayList<>();
+		for (AddonInfo info : getAllAddons().keySet()) {
+			handleDependencies(visited, loadOrder, info);
 		}
 		return loadOrder;
 	}
 
 	//TODO: do we want to split this up between init and register?
 	default void launchAll() {
-		getLoadOrder().forEach(spec -> {
-			if (!spec.getPlatformSupport(getPlatform()).canRun()) {
-				throw new IllegalStateException(String.format("Addon %s cannot run on platform %s!", spec.getAddonId(), getPlatform().toString()));
+		getLoadOrder().forEach(info -> {
+			if (!info.getPlatformSupport(getPlatform()).canRun()) {
+				throw new IllegalStateException(String.format("Addon %s cannot run on platform %s!", info.getAddonId(), getPlatform().toString()));
 			}
-			Addon addon = getAllAddons().get(spec);
+			Addon addon = getAllAddons().get(info);
 			if (addon != null) {
-				SandboxAPI api = getAPIFor(spec);
-				Registrar registrar = getRegistrarFor(spec);
+				SandboxAPI api = getAPIFor(info);
+				Registrar registrar = getRegistrarFor(info);
 				try {
 					addon.init(api);
 					addon.register(registrar);
 				} catch (Exception e) {
-					throw new RuntimeException(String.format("Initialization for addon %s failed: %s", spec.getAddonId(), e.getMessage()), e);
+					throw new RuntimeException(String.format("Initialization for addon %s failed: %s", info.getAddonId(), e.getMessage()), e);
 				}
 			} else {
 				//TODO: log that an addon has no entrypoint?
@@ -69,19 +69,19 @@ public interface Sandbox {
 	}
 
 	//TODO: does this properly prevent circular dependencies while satisfying everything?
-	default void handleDependencies(Set<AddonSpec> visited, List<AddonSpec> order, AddonSpec spec) {
-		if (visited.contains(spec)) return;
-		visited.add(spec);
-		Map<String, String> dependencies = spec.getDependencies();
+	default void handleDependencies(Set<AddonInfo> visited, List<AddonInfo> order, AddonInfo info) {
+		if (visited.contains(info)) return;
+		visited.add(info);
+		Map<String, String> dependencies = info.getDependencies();
 		for (String dep : dependencies.keySet()) {
-			Optional<AddonSpec> optionalDep = getAddon(dep);
-			if (!optionalDep.isPresent()) throw new IllegalStateException(String.format("Addon %s depends on other addon %s that isn't loaded!", spec.getAddonId(), dep));
-			AddonSpec dependency = optionalDep.get();
+			Optional<AddonInfo> optionalDep = getAddon(dep);
+			if (!optionalDep.isPresent()) throw new IllegalStateException(String.format("Addon %s depends on other addon %s that isn't loaded!", info.getAddonId(), dep));
+			AddonInfo dependency = optionalDep.get();
 			String versionString = dependencies.get(dep);
 			Expression version = PARSER.parse(versionString);
-			if (!version.interpret(dependency.getVersion())) throw new IllegalStateException(String.format("Addon %s depends on %s version %s but found version %s instead!", spec.getAddonId(), dep, versionString, dependency.getVersion().toString()));
-			handleDependencies(visited, order, spec);
+			if (!version.interpret(dependency.getVersion())) throw new IllegalStateException(String.format("Addon %s depends on %s version %s but found version %s instead!", info.getAddonId(), dep, versionString, dependency.getVersion().toString()));
+			handleDependencies(visited, order, info);
 		}
-		order.add(spec);
+		order.add(info);
 	}
 }
