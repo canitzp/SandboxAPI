bc2dd269734e057908ccb6a2d987e5115c1f04aa
Coded <coded@voxel.games>
2020-05-05 11:45:38 +0100

    Revert all meaningful changes

--- rendering-vulkan/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module sandbox.rendering.vulkan {
-    requires transitive sandbox.rendering;
-
-    uses org.sandboxpowered.api.client.render.RenderPipeline;
-
-    exports org.sandboxpowered.api.client.render.vulkan;
-}
