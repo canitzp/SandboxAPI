ffe79c87cb0429c5e81dd8cdea9aec7a1b2471b7
Coded <coded@voxel.games>
2020-06-25 12:15:48 +0100

    Add getRegistryIdentity

--- base/src/main/java/org/sandboxpowered/api/content/Content.java
+++ base/src/main/java/org/sandboxpowered/api/content/Content.java
@@ -6,6 +6,8 @@ import org.sandboxpowered.api.util.Identity;
 public interface Content<T extends Content<T>> {
     Class<T> getContentType();
 
+    Identity getRegistryIdentity();
+
     @SuppressWarnings("unchecked")
     default Identity getIdentity() {
         return Registry.getRegistryFromType(getContentType()).getIdentity((T) this);
