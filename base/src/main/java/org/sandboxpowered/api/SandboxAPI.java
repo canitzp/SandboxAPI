a4259c3aa89cbc88f9481a44660e9896f6925847
Coded <coded@voxel.games>
2020-07-15 20:54:34 +0100

    Add isLoaded checks to SandboxAPI

--- base/src/main/java/org/sandboxpowered/api/SandboxAPI.java
+++ base/src/main/java/org/sandboxpowered/api/SandboxAPI.java
@@ -1,10 +1,31 @@
 package org.sandboxpowered.api;
 
+import org.jetbrains.annotations.Nullable;
 import org.sandboxpowered.api.addon.AddonInfo;
+import org.sandboxpowered.api.util.Identity;
 import org.sandboxpowered.api.util.Log;
 import org.sandboxpowered.api.util.Side;
 
 public interface SandboxAPI {
+    /**
+     * Checks if a sandbox addon is loaded
+     *
+     * @param addonId the addonId to check
+     * @return if the addon is loaded
+     */
+    boolean isAddonLoaded(String addonId);
+
+    /**
+     * Checks if a non-sandbox mod is loaded
+     *
+     * E.G: fabric:reichenbach or forge:examplemod
+     *
+     * @param loader The loader to check
+     * @param modId The modId to check, if empty returns if the loader is enabled
+     * @return if the mod is loaded
+     */
+    boolean isExternalModLoaded(String loader, String modId);
+
     AddonInfo getSourceAddon();
 
     Side getSide();
