dca54df83b905da7acacfcf902122ee87d46674b
Coded <coded@voxel.games>
2020-07-29 10:54:54 +0100

    Add new canReplace method

--- modules/base/src/main/java/org/sandboxpowered/api/state/BlockState.java
+++ modules/base/src/main/java/org/sandboxpowered/api/state/BlockState.java
@@ -5,11 +5,15 @@ import org.sandboxpowered.api.block.Block;
 import org.sandboxpowered.api.block.Material;
 import org.sandboxpowered.api.client.GraphicsMode;
 import org.sandboxpowered.api.component.Component;
+import org.sandboxpowered.api.entity.player.Hand;
+import org.sandboxpowered.api.entity.player.PlayerEntity;
+import org.sandboxpowered.api.item.ItemStack;
 import org.sandboxpowered.api.util.Direction;
 import org.sandboxpowered.api.util.Mirror;
 import org.sandboxpowered.api.util.Mono;
 import org.sandboxpowered.api.util.Rotation;
 import org.sandboxpowered.api.util.math.Position;
+import org.sandboxpowered.api.util.math.Vec3d;
 import org.sandboxpowered.api.world.WorldReader;
 
 public interface BlockState extends PropertyContainer<BlockState> {
@@ -23,8 +27,18 @@ public interface BlockState extends PropertyContainer<BlockState> {
         return getBlock().getPistonInteraction(this);
     }
 
+    /**
+     *
+     * @deprecated
+     * <p> Use {@link BlockState#canReplace(WorldReader, Position, PlayerEntity, Hand, ItemStack, Direction, Vec3d)} instead.
+     */
+    @Deprecated
     default boolean canReplace() {
         return getBlock().canReplace(this);
+    }
+
+    default boolean canReplace(WorldReader reader, Position pos, PlayerEntity player, Hand hand, ItemStack stack, Direction side, Vec3d hitPos) {
+        return getBlock().canReplace(reader, pos, this, player, hand, stack, side, hitPos);
     }
 
     default Block.BlockRenderLayer getRenderLayer(GraphicsMode mode) {
@@ -65,5 +79,9 @@ public interface BlockState extends PropertyContainer<BlockState> {
 
     default int getStrongPower(WorldReader blockView, Position pos, Direction direction) {
         return getBlock().getStrongPower(blockView, pos, this, direction);
+    }
+
+    default boolean is(Block block) {
+        return getBlock().isSame(block) || block.isSame(getBlock());
     }
 }
