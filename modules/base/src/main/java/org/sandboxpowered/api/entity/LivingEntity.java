1a801d7b6cb7e8399f3f6a9e414bf4f2b40a672f
Coded <coded@voxel.games>
2020-08-17 19:47:00 +0100

    Improve entity methods

--- modules/base/src/main/java/org/sandboxpowered/api/entity/LivingEntity.java
+++ modules/base/src/main/java/org/sandboxpowered/api/entity/LivingEntity.java
@@ -1,5 +1,7 @@
 package org.sandboxpowered.api.entity;
 
+import org.sandboxpowered.api.entity.player.Hand;
+import org.sandboxpowered.api.item.ItemStack;
 import org.sandboxpowered.api.util.annotation.Alpha;
 
 @Alpha
@@ -7,4 +9,50 @@ public interface LivingEntity extends Entity {
     float getHealth();
 
     void setHealth(float health);
+
+    default boolean isHoldingItem(Hand hand) {
+        return !getHeld(hand).isEmpty();
+    }
+
+    void setHeld(Hand hand, ItemStack stack);
+
+    ItemStack getHeld(Hand hand);
+
+    default void swing(Hand hand) {
+        swing(hand, false);
+    }
+
+    void swing(Hand hand, boolean updateSelf);
+
+    default boolean hasEquipped(EquipmentSlot slot) {
+        return !getEquipped(slot).isEmpty();
+    }
+
+    ItemStack getEquipped(EquipmentSlot slot);
+
+    Void equip(EquipmentSlot slot, ItemStack stack);
+
+    enum EquipmentSlot {
+        MAINHAND(Type.HAND),
+        OFFHAND(Type.HAND),
+        FEET(Type.ARMOR),
+        LEGS(Type.ARMOR),
+        CHEST(Type.ARMOR),
+        HEAD(Type.ARMOR);
+
+        private final EquipmentSlot.Type type;
+
+        EquipmentSlot(Type type) {
+            this.type = type;
+        }
+
+        public Type getType() {
+            return type;
+        }
+
+        enum Type {
+            HAND,
+            ARMOR
+        }
+    }
 }
