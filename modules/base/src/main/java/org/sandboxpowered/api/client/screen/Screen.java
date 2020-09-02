1f06111694b46a81c0c5c0c6c87aeaa90cda04ff
Coded <coded@voxel.games>
2020-09-02 11:48:43 +0100

    Add requested changes

--- modules/base/src/main/java/org/sandboxpowered/api/client/screen/Screen.java
+++ modules/base/src/main/java/org/sandboxpowered/api/client/screen/Screen.java
@@ -2,11 +2,12 @@ package org.sandboxpowered.api.client.screen;
 
 import org.sandboxpowered.api.client.Client;
 import org.sandboxpowered.api.util.math.MatrixStack;
+import org.sandboxpowered.api.util.text.Text;
 
 public interface Screen {
     void init(Client client, int screenWidth, int screenHeight);
 
-    void render(Client client, MatrixStack matrixStack, int mouseX, int mouseY, float partial);
+    void render(Client client, MatrixStack matrixStack, int mouseX, int mouseY, float delta);
 
     default boolean shouldPauseGame() {
         return true;
@@ -20,5 +21,9 @@ public interface Screen {
         return true;
     }
 
-    String getNarratorText();
+    Text getTitle();
+
+    default Text getNarratorText() {
+        return getTitle();
+    }
 }
