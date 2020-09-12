8da1f6b036a6de2d9ae66109fa9c0ec6c57289d4
Coded <coded@voxel.games>
2020-09-12 08:51:52 +0100

    Add rendering methods to dynamic renderer

--- modules/rendering/src/main/java/org/sandboxpowered/api/client/rendering/ui/DynamicRenderer.java
+++ modules/rendering/src/main/java/org/sandboxpowered/api/client/rendering/ui/DynamicRenderer.java
@@ -1,7 +1,10 @@
 package org.sandboxpowered.api.client.rendering.ui;
 
+import org.sandboxpowered.api.client.rendering.VertexConsumer;
+import org.sandboxpowered.api.shape.Box;
 import org.sandboxpowered.api.util.Identity;
 import org.sandboxpowered.api.util.math.MatrixStack;
+import org.sandboxpowered.api.util.math.Vec3f;
 
 public interface DynamicRenderer {
     void renderBox(MatrixStack stack, int color, int posX, int posY, int sizeX, int sizeY);
@@ -41,6 +44,10 @@ public interface DynamicRenderer {
     default void renderSpriteArray(MatrixStack stack, Sprite sprite, int posX, int posY, int columns, int rows) {
         renderSpriteArray(stack, sprite, posX, posY, sprite.getSizeX(), sprite.getSizeY(), columns, rows);
     }
+
+    void drawBox(MatrixStack stack, VertexConsumer consumer, Box box, float r, float g, float b);
+
+    void drawLine(MatrixStack stack, VertexConsumer consumer, Vec3f from, Vec3f to, float r, float g, float b);
 
     enum SliceDirection {
         UP,
