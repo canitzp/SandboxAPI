ff9064ef8105e55b0f388c8f4891c65dafff4fcb
Coded <coded@voxel.games>
2020-09-16 11:33:47 +0100

    Move to internal service creating event handlers

--- modules/base/src/main/java/org/sandboxpowered/api/events/EntityEvents.java
+++ modules/base/src/main/java/org/sandboxpowered/api/events/EntityEvents.java
@@ -6,12 +6,13 @@ import org.sandboxpowered.api.util.math.Position;
 import org.sandboxpowered.api.world.World;
 import org.sandboxpowered.eventhandler.EventHandler;
 import org.sandboxpowered.eventhandler.ResettableEventHandler;
+import org.sandboxpowered.internal.InternalService;
 
 import java.util.List;
 
 public final class EntityEvents {
-    public static final EventHandler<EntityEvent.Cancellable> SPAWN = new ResettableEventHandler<>();
-    public static final EventHandler<AnvilFallEvent> ANVIL_FALL = new ResettableEventHandler<>();
+    public static final EventHandler<EntityEvent.Cancellable> SPAWN = InternalService.getInstance().createEventHandler();
+    public static final EventHandler<AnvilFallEvent> ANVIL_FALL = InternalService.getInstance().createEventHandler();
 
     public interface AnvilFallEvent {
         void onEvent(World world, Position pos, BlockState fallingState, BlockState hitState, Entity entity, List<Entity> hitEntities);
--- modules/rendering/src/main/java/org/sandboxpowered/api/client/rendering/events/RenderEvents.java
+++ modules/rendering/src/main/java/org/sandboxpowered/api/client/rendering/events/RenderEvents.java
@@ -5,9 +5,10 @@ import org.sandboxpowered.api.util.math.MatrixStack;
 import org.sandboxpowered.api.world.World;
 import org.sandboxpowered.eventhandler.EventHandler;
 import org.sandboxpowered.eventhandler.ResettableEventHandler;
+import org.sandboxpowered.internal.InternalService;
 
 public final class RenderEvents {
-    public static final EventHandler<RenderInWorld> RENDER_IN_WORLD = new ResettableEventHandler<>();
+    public static final EventHandler<RenderInWorld> RENDER_IN_WORLD = InternalService.getInstance().createEventHandler();
 
     public interface RenderInWorld {
         void onEvent(World world, MatrixStack stack, VertexConsumer.Provider provider);
