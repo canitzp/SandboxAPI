a2f789c5b7246740058bbd9a468df191298e08e8
Coded <coded@voxel.games>
2020-09-21 13:49:55 +0100

    Start Dynamic Ingredients

--- modules/base/src/main/java/org/sandboxpowered/api/recipe/Ingredient.java
+++ modules/base/src/main/java/org/sandboxpowered/api/recipe/Ingredient.java
@@ -5,9 +5,22 @@ import com.google.gson.JsonElement;
 import java.util.function.Predicate;
 
 public interface Ingredient<S> extends Predicate<S> {
+    static <T> Ingredient<T> from(T t) {
+        return null;
+    }
+
+    @SafeVarargs
+    static <T> Ingredient<T> from(T... ts) {
+        return null;
+    }
+
     S[] getMatching();
 
     boolean isEmpty();
 
     JsonElement toJson();
+
+    interface Deserializer<T> {
+        Ingredient<T> fromJson(JsonElement element);
+    }
 }
