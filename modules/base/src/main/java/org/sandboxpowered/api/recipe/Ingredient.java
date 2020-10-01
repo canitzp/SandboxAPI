7bd76682b4558597606e749c252b895336708136
Coded <coded@voxel.games>
2020-10-01 13:47:17 +0100

    Add fromJson

--- modules/base/src/main/java/org/sandboxpowered/api/recipe/Ingredient.java
+++ modules/base/src/main/java/org/sandboxpowered/api/recipe/Ingredient.java
@@ -5,12 +5,16 @@ import com.google.gson.JsonElement;
 import java.util.function.Predicate;
 
 public interface Ingredient<S> extends Predicate<S> {
-    static <T> Ingredient<T> from(T t) {
+    static <T> Ingredient<T> of(T t) {
         return null;
     }
 
     @SafeVarargs
-    static <T> Ingredient<T> from(T... ts) {
+    static <T> Ingredient<T> of(T... ts) {
+        return null;
+    }
+
+    static <T> Ingredient<T> fromJson(Class<T> type, JsonElement element) {
         return null;
     }
 
