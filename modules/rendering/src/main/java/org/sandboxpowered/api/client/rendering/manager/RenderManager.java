beda176edc441767b782d16183cc7b290114f3a1
Ashley Wright <ahmedtrg981106@gmail.com>
2020-10-07 20:19:58 +0300

    Add renderer registering stuff

--- modules/rendering/src/main/java/org/sandboxpowered/api/client/rendering/manager/RenderManager.java
+++ modules/rendering/src/main/java/org/sandboxpowered/api/client/rendering/manager/RenderManager.java
@@ -2,11 +2,15 @@ package org.sandboxpowered.api.client.rendering.manager;
 
 import org.sandboxpowered.api.block.entity.BlockEntity;
 import org.sandboxpowered.api.client.rendering.entity.BlockEntityRenderer;
+import org.sandboxpowered.api.client.rendering.entity.EntityRenderer;
+import org.sandboxpowered.api.entity.Entity;
 import org.sandboxpowered.api.util.annotation.PreAlpha;
 
 import java.util.function.Supplier;
 
 @PreAlpha
 public interface RenderManager {
-    <T extends BlockEntity> void register(Class<T> beClass, Supplier<BlockEntityRenderer<T>> render);
+    void register(BlockEntity.Type type, Supplier<BlockEntityRenderer<?>> render);
+
+    void register(Entity.Type type, Supplier<EntityRenderer<?>> render);
 }
