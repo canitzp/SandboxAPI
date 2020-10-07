b92376788438e47d4320b7bd77576f862bda763b
Ashley Wright <ahmedtrg981106@gmail.com>
2020-10-07 20:32:42 +0300

    Add render layer to LegacyEntityModel

--- modules/rendering/src/main/java/org/sandboxpowered/api/client/rendering/model/LegacyEntityModel.java
+++ modules/rendering/src/main/java/org/sandboxpowered/api/client/rendering/model/LegacyEntityModel.java
@@ -1,9 +1,13 @@
 package org.sandboxpowered.api.client.rendering.model;
 
+import org.sandboxpowered.api.client.rendering.RenderLayer;
 import org.sandboxpowered.api.client.rendering.VertexConsumer;
 import org.sandboxpowered.api.entity.Entity;
 import org.sandboxpowered.api.internal.SandboxInternalRenderingService;
+import org.sandboxpowered.api.util.Identity;
 import org.sandboxpowered.api.util.math.MatrixStack;
+
+import java.util.function.Function;
 
 /**
  * Entity model that is designed to mimic the API most modelling programs will be able to export.
@@ -11,7 +15,16 @@ import org.sandboxpowered.api.util.math.MatrixStack;
  * <b>Should only be used if there is no other option.</b>
  */
 public abstract class LegacyEntityModel<T extends Entity> {
+    protected final Function<Identity, RenderLayer> layerFactory;
     protected int textureWidth = 256, textureHeight = 256;
+
+    protected LegacyEntityModel() {
+        this(RenderLayer::getCustomCutout);
+    }
+
+    protected LegacyEntityModel(Function<Identity, RenderLayer> layerFactory) {
+        this.layerFactory = layerFactory;
+    }
 
     public final Bone createBone(int textureX, int textureY) {
         return SandboxInternalRenderingService.getInstance().createBone(this).setTextureOffset(textureX, textureY);
@@ -20,6 +33,10 @@ public abstract class LegacyEntityModel<T extends Entity> {
     public abstract void setRotationAngles(T entity, float swing, float swingAmount, float age, float headYaw, float headPitch);
 
     public abstract void render(MatrixStack matrixStack, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha);
+
+    public RenderLayer getLayer(Identity texture) {
+        return layerFactory.apply(texture);
+    }
 
     public interface Bone {
         Bone setRotationPoint(float x, float y, float z);
